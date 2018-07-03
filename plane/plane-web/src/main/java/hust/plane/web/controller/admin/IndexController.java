b5c0bc628cf7002f1b869e9b29c9ab37fdcc506f
yrf <1261649009@qq.com>
2018-07-03 18:13:33 +0800

    Merge branch 'master' of https://github.com/hpOnUser/PlaneProject
    # Conflicts:
    #	plane/plane-web/src/main/webapp/WEB-INF/jsp/route.html
    增加用户头像显示和注销功能

--- plane/plane-web/src/main/java/hust/plane/web/controller/admin/IndexController.java
+++ plane/plane-web/src/main/java/hust/plane/web/controller/admin/IndexController.java
@@ -19,9 +19,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller("adminIndexController")
 @RequestMapping(value = "/admin")
@@ -34,13 +36,17 @@ public class IndexController {
 
     /**
      * 登陆Get请求
+     *
      * @return
      */
-    @RequestMapping(value ="/login" ,method = RequestMethod.GET)
-    public String login(){return "login";}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
     /**
      * 登陆
+     *
      * @param username
      * @param password
      * @param remeber_me
@@ -48,21 +54,21 @@ public class IndexController {
      * @param response
      * @return
      */
-    @RequestMapping(value = "/login",produces="application/json;charset=UTF-8",method = RequestMethod.POST)
+    @RequestMapping(value = "/login", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
     @ResponseBody
     public String doLogin(@RequestParam String username, @RequestParam String password, @RequestParam(required = false) String remeber_me,
-                            HttpServletRequest request, HttpServletResponse response){
+                          HttpServletRequest request, HttpServletResponse response) {
         Integer error_count = cache.get("login_error_count");
         try {
-            User user =userService.login(username,password);
-            request.getSession().setAttribute(WebConst.LOGIN_SESSION_KEY,user);
-            if(StringUtils.isNotBlank(remeber_me)){
-                PlaneUtils.setCookie(response,user.getUserid());
+            User user = userService.login(username, password);
+            request.getSession().setAttribute(WebConst.LOGIN_SESSION_KEY, user);
+            if (StringUtils.isNotBlank(remeber_me)) {
+                PlaneUtils.setCookie(response, user.getUserid());
             }
-        }catch (Exception e){
-            error_count=null==error_count?1:error_count+1;
-            if(error_count>3){
-                return JsonView.render(1,"您输入密码已经错误超过3次，请10分钟后尝试");
+        } catch (Exception e) {
+            error_count = null == error_count ? 1 : error_count + 1;
+            if (error_count > 3) {
+                return JsonView.render(1, "您输入密码已经错误超过3次，请10分钟后尝试");
             }
             cache.set("login_error_count", error_count, 10 * 60);
             String msg = "登录失败";
@@ -71,42 +77,64 @@ public class IndexController {
             } else {
                 LOGGER.error(msg, e);
             }
-            return JsonView.render(1,msg);
+            return JsonView.render(1, msg);
         }
         return JsonView.render("");
     }
+
     /**
      * 注册get请求
      */
-    @RequestMapping(value="/register",method = RequestMethod.GET)
-    public ModelAndView doRegister(ModelAndView mv){
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView doRegister(ModelAndView mv) {
         mv.setViewName("register");
         return mv;
     }
+
     /**
      * 注册post请求
      */
-    @RequestMapping(value = "/register",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/register", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public String doRegister(@RequestParam String username, @RequestParam String password, @RequestParam String passwordensure,
-                             HttpServletRequest request, HttpSession session){
-        try{
-               int count = userService.register(username,password);
-               if(count<0){
-                   return JsonView.render(1,"注册失败，请重新注册");
-               }
-        }catch(Exception e){
-              String msg = "注册失败";
-              if(e instanceof TipException){
-                  msg = e.getMessage();
-              }else{
-                  LOGGER.error(msg,e);
-              }
-              return JsonView.render(1,msg);
+                             HttpServletRequest request, HttpSession session) {
+        try {
+            int count = userService.register(username, password);
+            if (count < 0) {
+                return JsonView.render(1, "注册失败，请重新注册");
+            }
+        } catch (Exception e) {
+            String msg = "注册失败";
+            if (e instanceof TipException) {
+                msg = e.getMessage();
+            } else {
+                LOGGER.error(msg, e);
+            }
+            return JsonView.render(1, msg);
         }
-        return JsonView.render(0,"注册成功");
+        return JsonView.render(0, "注册成功");
     }
-
-
+    /**
+     * 用户退出登陆
+     * @author rfYang
+     * @date 2018/7/3 18:07
+     * @param [request, response, session]
+     * @return void
+     */
+    @RequestMapping(value = "/logout")
+    public void doLogout(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        session.removeAttribute(WebConst.LOGIN_SESSION_KEY);
+        Cookie cookie = new Cookie(WebConst.USER_IN_COOKIE, "");
+        cookie.setValue(null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        try {
+            response.sendRedirect("login");
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error("注销失败", e);
+        }
+    }
 
 }
