51592396c429c775d720d52b7dd22b6161410b68
hujunhui123 <hujunhui123@outlook.com>
2018-07-17 20:52:34 +0800

    修改任务列表和planeController

--- plane/plane-web/src/main/java/hust/plane/web/controller/PlaneController.java
+++ plane/plane-web/src/main/java/hust/plane/web/controller/PlaneController.java
@@ -9,11 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import hust.plane.mapper.pojo.Plane;
-import hust.plane.mapper.pojo.User;
 import hust.plane.service.interFace.PlaneService;
 import hust.plane.utils.JsonUtils;
 import hust.plane.utils.PointUtil;
