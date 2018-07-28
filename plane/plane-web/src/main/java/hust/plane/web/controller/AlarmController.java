a585e838f04eb4322b6b18014c5b7fe00c6741da
hujunhui123 <hujunhui123@outlook.com>
2018-07-28 18:00:06 +0800

    修改导入告警图片

--- plane/plane-web/src/main/java/hust/plane/web/controller/AlarmController.java
+++ plane/plane-web/src/main/java/hust/plane/web/controller/AlarmController.java
@@ -1,5 +1,6 @@
 package hust.plane.web.controller;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import hust.plane.mapper.pojo.Alarm;
 import hust.plane.mapper.pojo.Plane;
@@ -106,5 +108,44 @@ public class AlarmController {
           return JsonView.render(1,msg);
         }
          return JsonView.render(0,WebConst.SUCCESS_RESULT);
+    }
+    
+    
+    //导入告警图片
+    @RequestMapping(value = "importAlarmImg", method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String importAlarmImg(@RequestParam(value = "planeId") String planeId,@RequestParam("AlarmImgs") MultipartFile[] AlarmImgs,HttpServletRequest request) {
+    	
+    	List<String> list = new ArrayList<String>();
+        if (AlarmImgs != null && AlarmImgs.length > 0) {
+            for (int i = 0; i < AlarmImgs.length; i++) {
+                MultipartFile file = AlarmImgs[i];
+                // 保存文件
+                list = saveFile(request, file, list);
+            }
+        }       	
+    	return  "告警图片导入成功！";
+    }
+    
+    //保存图片
+    private List<String> saveFile(HttpServletRequest request,MultipartFile file, List<String> list) {
+        // 判断文件是否为空
+        if (!file.isEmpty()) {
+           try {
+                // 保存的文件路径(如果用的是Tomcat服务器，文件会上传到\\%TOMCAT_HOME%\\webapps\\YourWebProject\\upload\\文件夹中     
+        	    String filePath = request.getSession().getServletContext().getRealPath("/")+ "upload/" + file.getOriginalFilename();
+        	    //System.out.println(filePath);  这个保存路径是个问题啊
+                list.add(file.getOriginalFilename());
+                File saveDir = new File(filePath);
+                if (!saveDir.getParentFile().exists())
+                    saveDir.getParentFile().mkdirs();
+                // 转存文件
+                file.transferTo(saveDir);
+                return list;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
